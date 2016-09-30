cd1736fdb3d655151a13fc3e4df71627b12bc75b
muschellij2 <muschellij2@gmail.com>
2016-09-30 07:43:14 -0400

    adding files to repo

---
+++ R/get_asl_filenames.R
@@ -0,0 +1,15 @@
+#' @title Get ASL Image Filenames
+#'
+#' @description Return the filenames for the ASL images
+#' @param ... arguments to pass to \code{\link{get_image_filenames}},
+#' \code{modalities = "ASL"} so it cannot be specified
+#' @return Vector fo filenames
+#' 
+#' @examples
+#' get_asl_filenames()
+#' @export
+#' @importFrom kirby21.base get_image_filenames
+get_asl_filenames = function(...) {
+  x = kirby21.base::get_image_filenames(modalities = "ASL", ...)
+  return(x)
+}
