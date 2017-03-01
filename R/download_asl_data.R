59ab7a95fe2dea39d54011c5d928ef8eefdb885c
muschellij2 <muschellij2@gmail.com>
2017-03-01 15:12:06 -0500

    adding readme

--- R/download_asl_data.R
+++ R/download_asl_data.R
@@ -5,10 +5,8 @@
 #' \code{modalities = "ASL"} so it cannot be specified
 #' @return Indicator if the data is downloaded.
 #' 
-#' @examples
-#' download_asl_data()
 #' @export
-#' @importFrom kirby21.base download_kirby21_data
+#' @importFrom kirby21.base download_kirby21_data all_modalities
 download_asl_data =  function(...) {  
   x = kirby21.base::download_kirby21_data(modality = "ASL", ...)
   return(x)
